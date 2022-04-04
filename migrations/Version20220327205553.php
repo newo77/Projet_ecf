c0f1e53e4120bd164b08b53f6945c4207f591065
owen <newodayesiu@gmail.com>
2022-04-04 23:24:16 +0200

    Regroupement des migrations en BDD

--- migrations/Version20220327205553.php
+++ migrations/Version20220327205553.php
@@ -21,11 +21,32 @@ final class Version20220327205553 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD firstname VARCHAR(255) NOT NULL, ADD lastname VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE cours (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, chapitre VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cours ADD illustration VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cours ADD category_id INT NOT NULL, ADD slug VARCHAR(255) NOT NULL, ADD subtitle VARCHAR(255) NOT NULL, ADD description LONGTEXT NOT NULL, DROP is_best');
+        $this->addSql('ALTER TABLE cours ADD CONSTRAINT FK_FDCA8C9C12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_FDCA8C9C12469DE2 ON cours (category_id)');
+        $this->addSql('ALTER TABLE cours ADD is_best TINYINT(1) NOT NULL');
+        $this->addSql('CREATE TABLE header (id INT AUTO_INCREMENT NOT NULL, titre LONGTEXT NOT NULL, button VARCHAR(255) NOT NULL, btn_url VARCHAR(255) NOT NULL, illustration VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE header ADD contenu LONGTEXT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE user DROP firstname, DROP lastname');
+        $this->addSql('DROP TABLE cours');
+        $this->addSql('ALTER TABLE cours DROP illustration');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('ALTER TABLE cours DROP FOREIGN KEY FK_FDCA8C9C12469DE2');
+        $this->addSql('DROP INDEX IDX_FDCA8C9C12469DE2 ON cours');
+        $this->addSql('ALTER TABLE cours ADD is_best TINYINT(1) NOT NULL, DROP category_id, DROP slug, DROP subtitle, DROP description');
+        $this->addSql('ALTER TABLE cours DROP is_best');
+        $this->addSql('DROP TABLE header');
+        $this->addSql('ALTER TABLE header DROP contenu');
+
     }
 }
